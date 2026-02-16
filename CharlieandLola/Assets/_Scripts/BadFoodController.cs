1583bf5f15c88c86a88d906746e7052d6399449e
srousserpitt <SAR714@pitt.edu>
2026-02-16 16:14:11 -0500

    Class Workshop
    Generated scenes, added assets

---
+++ CharlieandLola/Assets/_Scripts/BadFoodController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BadFoodController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Delete if object hits floor
+        if (gameObject.transform.position.y <= -20f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // What happens when the player collides with the food
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            //Debug.Log("Collided with bad food");
+            Destroy(gameObject);
+            //TODO: Decrease score
+        }
+    }
+}
---
+++ CharlieandLola/Assets/_Scripts/GoodFoodController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoodFoodController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Delete if object hits floor
+        if(gameObject.transform.position.y <= -20f)
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+    // What happens when the player collides with the food
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            //Debug.Log("Collided with good food");
+            Destroy(gameObject);
+            //TODO: Increment score
+        }
+    }
+}
